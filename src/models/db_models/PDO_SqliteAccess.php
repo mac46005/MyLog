68169df8b90c1da4236522775a10bd1a84dd4d58
Marco Preciado <mac46005@gmail.com>
2022-09-07 18:49:22 -0500

    Continue working on PDO_SqliteAccess

---
+++ src/models/db_models/PDO_SqliteAccess.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace MyLog_ClassLib\Models\DB_Models;
+
+use MyLog_ClassLib\Models\DB_Models\Interfaces\IDBAccess;
+
+abstract class PDO_SqliteAccess implements IDBAccess{
+
+}
