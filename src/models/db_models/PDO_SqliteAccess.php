95579defc932a729891b5c30e5ed8ee191590f55
Marco Preciado <mac46005@gmail.com>
2022-09-07 23:23:47 -0500

    PDO_SgliteAccess

--- src/models/db_models/PDO_SqliteAccess.php
+++ src/models/db_models/PDO_SqliteAccess.php
@@ -4,6 +4,7 @@ declare(strict_types = 1);
 
 namespace MyLog_ClassLib\Models\DB_Models;
 
+use MyLog_ClassLib\Models\DB_Models\Enums\DBIniFile_Enum;
 use MyLog_ClassLib\Models\DB_Models\Interfaces\IDBAccess;
 
 abstract class PDO_SqliteAccess implements IDBAccess{
@@ -12,13 +13,26 @@ abstract class PDO_SqliteAccess implements IDBAccess{
         protected string $configFilePath = ''
     )
     {
-        
+        $this->connect();
     }
 
     public function connect()
     {
         if($dbIniFile = parse_ini_file($this->configFilePath)){
-            if($filepath = $dbIniFile[])
+            if($filepath = $dbIniFile[DBIniFile_Enum::OPTIONS]){
+                $this->db = new \PDO("sqlite:" . __DIR__ . '/../' . $filepath);
+            }else{
+                //throw new DB_IniConfigException();
+            }
+        }else{
+            throw new \PDOException("Failed to connect to sqlite file");
+        }
+    }
+
+    public function __destruct()
+    {
+        if($this->db){
+            $this->db = NULL;
         }
     }
 }
