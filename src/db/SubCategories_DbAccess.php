e41d29ad3d98818d52357c37d23ee2e65f50ffea
Marco Preciado <mac46005@gmail.com>
2022-09-09 09:23:07 -0500

    created another table for app

---
+++ src/db/SubCategories_DbAccess.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace MyLog_ClassLib\DB;
+
+class SubCategories_DbAccess extends PDO_SqliteAccess{
+
+    public function readOne($id): mixed
+    {
+        
+    }
+
+    public function readAll(): mixed
+    {
+        
+    }
+
+    public function write($obj): bool
+    {
+        
+    }
+
+    public function delete($id): bool
+    {
+        
+    }
+    public function update($obj): bool
+    {
+        
+    }
+
+    public function query($sql): mixed
+    {
+        
+    }
+
+}
